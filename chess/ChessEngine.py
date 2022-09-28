1dcec08122cfa417ed3fba9e437ce9e49c7885e7
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-09-28 19:56:57 -0300

    Renomeando as peças

---
+++ chess/ChessEngine.py
@@ -0,0 +1,4 @@
+"""
+Esta é a classe responsável por guardar toda a informação sobre o estado atual de uma partida de xadrez. Ele também será responsável por determinar os movimentos válidos no estado atual. Ele também irá registrar um log de movimentos.
+"""
+
---
+++ chess/ChessMain.py
@@ -0,0 +1,4 @@
+"""
+Este é o arquivo de driver principal. Ele ficará responsável por gerenciar input do usuário e disponibilizar o GameState object atual.
+"""
+
